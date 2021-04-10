743dc86b224f89b0dc4c689234ed2d15134f7d47
santosh-bitra <santosh.thebitra@gmail.com>
2021-04-10 18:52:37 +0530

    Python Project 1.1

---
+++ BandNameGenerator.py
@@ -0,0 +1,12 @@
+#Program to generate your new band name.
+'''This script will ask two questions, will input both and combine to form some really lame Band
+name in a really lame and straight forward way. But hey, this is my 1st python project so, cheers.!'''
+
+#giving city name into the variable x to contain
+x = input("What is the name of the city you're from? " '\n')
+
+#giving pet name into the variable y to contain
+y = input("What is the name of your pet ? " '\n')
+
+#the band name is just x with y beside and an empty space added in between
+print("Your new band could be called ", x + " " + y)
